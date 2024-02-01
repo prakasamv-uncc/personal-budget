a077842b6f76827309175bce4ea9d998f42e7db9
prakasamv-uncc <pvenkat6@uncc.edu>
2024-02-01 18:55:25 -0500

    Loading data from JSON file
    Loading data from JSON file

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+// Budget API
+
+const express = require('express');
+const cors = require('cors');
+const fs= require('fs');    // File system
+const path = require('path');    // Path module
+const app = express();
+const port = 3001;
+
+app.use(cors());
+
+ const budget = JSON.parse(fs.readFileSync(`${__dirname}/budget.json`, 'utf8'));
+
+app.get('/budget', (req, res) => {
+    res.json(budget);
+});
+
+app.listen(port, () => {
+    console.log(`API served at http://137.184.110.255:${port}`);
+});
