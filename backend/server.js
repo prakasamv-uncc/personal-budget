f38a9a136f4704845c2d6dcdf4248b91f938bec7
prakasamv-uncc <pvenkat6@uncc.edu>
2024-03-01 13:23:40 -0500

    Loading Data from MongoDB
    Loading Data from MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 // Budget API
 
-const express = require('express');
+/* const express = require('express');
 const cors = require('cors');
 const fs= require('fs');    // File system
 const path = require('path');    // Path module
@@ -8,7 +8,7 @@ const app = express();
 const port = 3001;
 
 app.use(cors());
-/* Loading JSON from file using file system  */
+//Loading JSON from file using file system  
  const budget = JSON.parse(fs.readFileSync(`${__dirname}/budget.json`, 'utf8'));
 
 app.get('/budget', (req, res) => {
@@ -17,4 +17,42 @@ app.get('/budget', (req, res) => {
 
 app.listen(port, () => {
     console.log(`API served at http://137.184.110.255:${port}`);
-});
+}); */
+
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+const port = 3001;
+bodyParser.urlencoded({ extended: true });
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/budget', (req, res) => {
+  try {
+  const uri = 'mongodb+srv://pvenkat6:6TaCSPSr5MSp8Kar@cluster0.ipw5bph.mongodb.net/';
+  const client = new MongoClient(uri);
+    client.connect();
+    client.db('personal_budget').collection('budget').find().toArray()
+    .then(result => {
+      client.close();
+        res.json(result);
+        
+    })
+    .catch(error => console.error('Error retrieving data from MongoDB:', error));
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+}
+);
+
+
+  
+  // Start the server
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
